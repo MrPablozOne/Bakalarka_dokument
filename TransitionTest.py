40d3cceb95df6f4ac11f772185419ccf6f77acaa
MrPablozOne <pavelsiemko@centrum.cz>
2014-09-01 17:38:09 +0200

    První spuštění funkce z context menu přechodu!

---
+++ TransitionTest.py
@@ -0,0 +1,41 @@
+#
+#    Copyright (C) 2014 Pavel Siemko
+#
+#    This file is part of Kaira.
+#
+#    Kaira is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, version 3 of the License, or
+#    (at your option) any later version.
+#
+#    Kaira is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with Kaira.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+#Pokus o unittest libovolneho prechodu
+
+
+import neteditor
+
+
+
+class TransitionTest:
+
+
+    def __init__(self):
+        print("ahoj")
+
+    def __init__(self,neteditor,project):
+        self.neteditor = neteditor
+        self.project = project
+        #self.netlist = self.neteditor.get_net()
+        #self.netlist._add_net()
+
+
+def doTest():
+    print("ahoj2")
